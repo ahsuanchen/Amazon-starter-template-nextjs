3e93aaa0403780204d0a7b18106187f2e069b583
FA00248\Brian Chen <brian.chen@shoalter.com>
2021-09-29 14:37:36 +0800

    new client

--- firebase.js
+++ firebase.js
@@ -2,11 +2,10 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAnA5oaZUIDq-2uDFg9Yy4ibET7zUJfwtk",
-  authDomain: "to-220e5.firebaseapp.com",
-  projectId: "to-220e5",
-  storageBucket: "to-220e5.appspot.com",
-  messagingSenderId: "675564780375",
-  appId: "1:675564780375:web:69c52d3bbd3ee4296e1c12",
-  measurementId: "G-7WTWVG9W4L",
+  apiKey: "AIzaSyAq6pZcHRsd2Gmgjh1ViROgDfOy7ZDrxn0",
+  authDomain: "to-89c27.firebaseapp.com",
+  projectId: "to-89c27",
+  storageBucket: "to-89c27.appspot.com",
+  messagingSenderId: "511118896432",
+  appId: "1:511118896432:web:32798de3bc7d6f08be2cf5",
 };
