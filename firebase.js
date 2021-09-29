eacd1752cece14cb9fb6a483ef642edb31e9d01a
FA00248\Brian Chen <brian.chen@shoalter.com>
2021-09-29 14:00:13 +0800

    day 3 done

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAnA5oaZUIDq-2uDFg9Yy4ibET7zUJfwtk",
+  authDomain: "to-220e5.firebaseapp.com",
+  projectId: "to-220e5",
+  storageBucket: "to-220e5.appspot.com",
+  messagingSenderId: "675564780375",
+  appId: "1:675564780375:web:69c52d3bbd3ee4296e1c12",
+  measurementId: "G-7WTWVG9W4L",
+};
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
 import Image from "next/image";
 import { StarIcon } from "@heroicons/react/solid";
 import Currency from "react-currency-formatter";
@@ -7,11 +9,29 @@ const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 function Product({ id, title, price, description, category, image }) {
+  const dispatch = useDispatch();
+
   const [rating] = useState(
     Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
   );
 
   const [hasPrime] = useState(Math.random() < 0.5);
+
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      rating,
+      price,
+      description,
+      category,
+      image,
+      hasPrime,
+    };
+
+    // Sending the product as an action to the REDUX store ... the basket slice
+    dispatch(addToBasket(product));
+  };
 
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
@@ -19,17 +39,17 @@ function Product({ id, title, price, description, category, image }) {
         {category}
       </p>
       <Image src={image} height={200} width={200} objectFit="contain" />
-      <h4 className="my-3 text-yellow-500">{title}</h4>
+      <h4 className="my-3">{title}</h4>
       <div className="flex">
         {Array(rating)
           .fill()
           .map((_, i) => (
-            <StarIcon className="h-5" />
+            <StarIcon key={i} className="h-5 text-yellow-500" />
           ))}
       </div>
       <p className="text-xs my-2 line-clamp-2">{description}</p>
       <div className="mb-5">
-        <Currency quantity={price} currency="GBP" />
+        <Currency quantity={price} currency="USD" />
       </div>
       {hasPrime && (
         <div className="flex items-center space-x-2 -mt-5">
@@ -41,7 +61,9 @@ function Product({ id, title, price, description, category, image }) {
           <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
       )}
-      <button className="button">Add to Basket</button>
+      <button onClick={addItemToBasket} className="mt-auto button">
+        Add to Basket
+      </button>
     </div>
   );
 }
